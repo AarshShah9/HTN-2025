6abc9635de94a0d5587108003605ea8945be5c1b
Osman Sultan <osmansultan2002@gmail.com>
2025-09-13 04:09:15 -0400

    fixes

--- backend/app/routers/image.py
+++ backend/app/routers/image.py
@@ -1,15 +1,12 @@
-from datetime import datetime
 from typing import List
-from uuid import uuid4
 
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter
 
-from ..config import supabase
-from ..models import Task, TaskCreate
+from ..models import Image
 
 router = APIRouter(prefix="/image", tags=["image"])
 
 
 @router.get("/", response_model=List[Image])
-def get_images(user_id: str):
+def get_images(image_id: str):
     return []
